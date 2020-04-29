1c24d2bcc935b3f46c0de803bba1d748669e04c7
Silzhar <luis.ruizfer@gmail.com>
2020-04-29 21:11:21 +0200

    fin 1ª parte /end back con JS

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
 var http =require ('http');
-var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
 var {info, error} = require('./modules/my-log');
+var {countries} = require('countries-list');
 
 var server =  http.createServer((function(request, response){
-    
-    if(request.url==='/'){
 
+    var parsed = url.parse(request.url);
+    console.log('parsed :',parsed);
+
+    var pathname = parsed.pathname;
+    var query = querystring.parse(parsed.query);
+    console.log('Query :',query);
+
+    
+    if(pathname==='/'){
         response.writeHead(200,{'Content-Type':'text/html'});  // Confirmación de conexión.
         response.write('<html><body><p>HOME PAGE</p></body></html>');
         response.end();
     }
-    else if(request.url==='/exit'){
-
+    else if(pathname==='/exit'){
         response.writeHead(200,{'Content-Type':'text/html'});  
         response.write('<html><body><p>BYE</p></body></html>');
         response.end();
     }
-    else if(request.url==='/info'){
-        var result = info(request.url);
+    else if(pathname==='/country'){
+        response.writeHead(200,{'Content-Type':'application/json'});  
+        response.write(JSON.stringify(countries[query.code]));
+        response.end();
+    }
+    else if(pathname==='/info'){
+        var result = info(pathname);
         response.writeHead(200,{'Content-Type':'text/html'});  
         response.write(result);
         response.end();
-    }else if(request.url==='/error'){
-        var result = error(request.url);
+    }else if(pathname==='/error'){
+        var result = error(pathname);
         response.writeHead(200,{'Content-Type':'text/html'});  
         response.write(result);
         response.end();
