1e54d95fdf15d8b5da55e0671ae80c16659c0d7f
Silzhar <luis.ruizfer@gmail.com>
2020-04-30 13:42:23 +0200

    pack nodemon / autorreinicio para cambios de proyecto

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ app.get('/', function(request, response){
     response.status(200).send('HELLO');
 });
 
+app.get('/info', function(request, response){
+    response.send('info nodemon');
+})
+
+app.get('*', function(request, response){
+    response.status(404).send('NOT FOUND');
+});
+
 // var server =  http.createServer((function(request, response){
 
 //     var parsed = url.parse(request.url);
