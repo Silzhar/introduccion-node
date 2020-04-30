248367ac19ded332cf816bdc9dd38806c33b8f1b
Silzhar <luis.ruizfer@gmail.com>
2020-04-30 13:20:23 +0200

    version express JS

--- src/index.js
+++ src/index.js
@@ -1,53 +1,64 @@
-var http =require ('http');
-var url = require('url');
-var querystring = require('querystring');
+const express = require('express');
+const app = express();
+
+// var http =require ('http');
+// var url = require('url');
+// var querystring = require('querystring');
 var {info, error} = require('./modules/my-log');
 var {countries} = require('countries-list');
 
-var server =  http.createServer((function(request, response){
+app.get('/', function(request, response){
+    response.status(200).send('HELLO');
+});
 
-    var parsed = url.parse(request.url);
-    console.log('parsed :',parsed);
+// var server =  http.createServer((function(request, response){
 
-    var pathname = parsed.pathname;
-    var query = querystring.parse(parsed.query);
-    console.log('Query :',query);
+//     var parsed = url.parse(request.url);
+//     console.log('parsed :',parsed);
 
-    
-    if(pathname==='/'){
-        response.writeHead(200,{'Content-Type':'text/html'});  // Confirmación de conexión.
-        response.write('<html><body><p>HOME PAGE</p></body></html>');
-        response.end();
-    }
-    else if(pathname==='/exit'){
-        response.writeHead(200,{'Content-Type':'text/html'});  
-        response.write('<html><body><p>BYE</p></body></html>');
-        response.end();
-    }
-    else if(pathname==='/country'){
-        response.writeHead(200,{'Content-Type':'application/json'});  
-        response.write(JSON.stringify(countries[query.code]));
-        response.end();
-    }
-    else if(pathname==='/info'){
-        var result = info(pathname);
-        response.writeHead(200,{'Content-Type':'text/html'});  
-        response.write(result);
-        response.end();
-    }else if(pathname==='/error'){
-        var result = error(pathname);
-        response.writeHead(200,{'Content-Type':'text/html'});  
-        response.write(result);
-        response.end();
-    }
-    else{
-        response.writeHead(404,{'Content-Type':'text/html'});
-        response.write('<html><body><p>NOT FOUND</p></body></html>');
-        response.end();
-    }
+//     var pathname = parsed.pathname;
+//     var query = querystring.parse(parsed.query);
+//     console.log('Query :',query);
 
     
-}));
+//     if(pathname==='/'){
+//         response.writeHead(200,{'Content-Type':'text/html'});  // Confirmación de conexión.
+//         response.write('<html><body><p>HOME PAGE</p></body></html>');
+//         response.end();
+//     }
+//     else if(pathname==='/exit'){
+//         response.writeHead(200,{'Content-Type':'text/html'});  
+//         response.write('<html><body><p>BYE</p></body></html>');
+//         response.end();
+//     }
+//     else if(pathname==='/country'){
+//         response.writeHead(200,{'Content-Type':'application/json'});  
+//         response.write(JSON.stringify(countries[query.code]));
+//         response.end();
+//     }
+//     else if(pathname==='/info'){
+//         var result = info(pathname);
+//         response.writeHead(200,{'Content-Type':'text/html'});  
+//         response.write(result);
+//         response.end();
+//     }else if(pathname==='/error'){
+//         var result = error(pathname);
+//         response.writeHead(200,{'Content-Type':'text/html'});  
+//         response.write(result);
+//         response.end();
+//     }
+//     else{
+//         response.writeHead(404,{'Content-Type':'text/html'});
+//         response.write('<html><body><p>NOT FOUND</p></body></html>');
+//         response.end();
+//     }
 
-server.listen(4000);
-console.log('running on 4000');
+    
+// }));
+
+app.listen(4000,function(){
+    console.log('running on 4000');
+    
+})
+// server.listen(4000);
+// console.log('running on 4000');
