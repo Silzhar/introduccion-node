be6ae985f4f295fec7ed3d562c2ace92ee95a3bf
Silzhar <luis.ruizfer@gmail.com>
2020-05-01 20:26:42 +0200

    qyery params

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-// var http =require ('http');
-// var url = require('url');
-// var querystring = require('querystring');
 var {info, error} = require('./modules/my-log');
-var {countries} = require('countries-list');
+var {countries, languages} = require('countries-list');
 
 app.get('/', function(request, response){
     response.status(200).send('HELLO');
@@ -15,64 +12,27 @@ app.get('/info', function(request, response){
     response.send('info nodemon');
 });
 
-app.get('/country',(request, response) => {
+app.get('/country', function(request, response){
     console.log('request.query :',request.query);
     response.json(countries[request.query.code]);
+});
+
+app.get('/languages/:lang', function(request, response){
+    console.log('request.params :',request.params);
+    var lang =  languages[request.params.lang];
+    if (lang){
+        response.json({status: 'OK', data: lang});
+    } else {
+        response.status(404).json({ status: 'NOT FOUND',
+        message: 'language '+ request.params.lang + ' not found'});
+    }
+    
 });
 
 app.get('*', function(request, response){
     response.status(404).send('NOT FOUND');
 });
 
-// var server =  http.createServer((function(request, response){
-
-//     var parsed = url.parse(request.url);
-//     console.log('parsed :',parsed);
-
-//     var pathname = parsed.pathname;
-//     var query = querystring.parse(parsed.query);
-//     console.log('Query :',query);
-
-    
-//     if(pathname==='/'){
-//         response.writeHead(200,{'Content-Type':'text/html'});  // Confirmación de conexión.
-//         response.write('<html><body><p>HOME PAGE</p></body></html>');
-//         response.end();
-//     }
-//     else if(pathname==='/exit'){
-//         response.writeHead(200,{'Content-Type':'text/html'});  
-//         response.write('<html><body><p>BYE</p></body></html>');
-//         response.end();
-//     }
-//     else if(pathname==='/country'){
-//         response.writeHead(200,{'Content-Type':'application/json'});  
-//         response.write(JSON.stringify(countries[query.code]));
-//         response.end();
-//     }
-//     else if(pathname==='/info'){
-//         var result = info(pathname);
-//         response.writeHead(200,{'Content-Type':'text/html'});  
-//         response.write(result);
-//         response.end();
-//     }else if(pathname==='/error'){
-//         var result = error(pathname);
-//         response.writeHead(200,{'Content-Type':'text/html'});  
-//         response.write(result);
-//         response.end();
-//     }
-//     else{
-//         response.writeHead(404,{'Content-Type':'text/html'});
-//         response.write('<html><body><p>NOT FOUND</p></body></html>');
-//         response.end();
-//     }
-
-    
-// }));
-
-app.listen(4000,function(){
+app.listen(4000, function(){
     console.log('running on 4000');
-    
 });
-
-// server.listen(4000);
-// console.log('running on 4000');
